5be90a21289b4bfec882417dca6c41a5cf2025b6
Nikolls06 <125685519+Nikolls06@users.noreply.github.com>
2024-11-12 14:20:20 -0300

    .

--- Astro/src/main/java/com/example/Astro/AstroApplication.java
+++ Astro/src/main/java/com/example/Astro/AstroApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AstroApplication {
+public class
+AstroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AstroApplication.class, args);
